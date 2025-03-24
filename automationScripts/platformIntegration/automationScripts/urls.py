83c8f17310479371bc389c9f449e77fac3d848b5
Subhash Raja <58407744+subhash1612@users.noreply.github.com>
2025-03-24 18:44:48 +0530

    Update urls.py

--- automationScripts/platformIntegration/automationScripts/urls.py
+++ automationScripts/platformIntegration/automationScripts/urls.py
@@ -4,4 +4,5 @@ from .views import UpdateGitHubView, ProcessDataView
 urlpatterns = [
     path('update-github/', UpdateGitHubView.as_view(), name='update_github'),
     path('process-data/', ProcessDataView.as_view(), name='process_data'),
+    path('create-file/', createFileView.as_view(), name='create_file'),
 ]
